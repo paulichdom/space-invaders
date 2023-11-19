0eb7bcd0533e3e1bcda60396bbc0281a76dd8e97
Domagoj <paulic.domagoj@gmail.com>
2023-11-19 12:13:32 +0100

    add game and player objects

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 100;
+    this.x = this.game.width * 0.5 - this.width * 0.5;
+    this.y = this.game.height - this.height;
+    this.speed = 5;
+  }
 
+  draw(context) {
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    this.x += this.speed;
+  }
 }
 
-class Projectile {
+class Projectile {}
 
-}
-
-class Enemy {
-
-}
+class Enemy {}
 
 class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.player = new Player(this);
+  }
 
+  render(context) {
+    this.player.draw(context);
+    this.player.update();
+  }
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = 600;
   canvas.height = 800;
-})
+
+  const game = new Game(canvas);
+  game.render(ctx);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
