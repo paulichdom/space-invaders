8240884ed6a9c53b70392ab728078e17be0d6120
Domagoj <paulic.domagoj@gmail.com>
2023-11-21 20:01:46 +0100

    add projectile object pool

--- script.js
+++ script.js
@@ -14,16 +14,53 @@ class Player {
 
   update() {
     // horizontal movement
-    if(this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
-    if(this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+    if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+    if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
 
     // horizontal boundaries
-    if(this.x < 0) this.x = 0;
-    if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+    if (this.x < -this.width * 0.5) this.x = -this.width * 0.5;
+    else if (this.x > this.game.width - this.width * 0.5)
+      this.x = this.game.width - this.width * 0.5;
+  }
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile) projectile.start(this.x + this.width * 0.5, this.y);
   }
 }
 
-class Projectile {}
+class Projectile {
+  constructor() {
+    this.width = 8;
+    this.height = 20;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 20;
+    this.free = true;
+  }
+
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+      if(this.y < -this.height) this.reset();
+    }
+  }
+
+  start(playerX, playerY) {
+    this.x = playerX - this.width * 0.5;
+    this.y = playerY;
+    this.free = false;
+  }
+
+  reset() {
+    this.free = true;
+  }
+}
 
 class Enemy {}
 
@@ -35,20 +72,44 @@ class Game {
     this.keys = [];
     this.player = new Player(this);
 
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
+    console.log(this.projectilesPool);
+
     // event listeners
-    window.addEventListener('keydown', e => {
+    window.addEventListener('keydown', (e) => {
       if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+      if (e.key === '1') this.player.shoot();
     });
 
-    window.addEventListener('keyup', e => {
+    window.addEventListener('keyup', (e) => {
       const index = this.keys.indexOf(e.key);
-      if(index > -1) this.keys.splice(index, 1);
+      if (index > -1) this.keys.splice(index, 1);
     });
   }
 
   render(context) {
     this.player.draw(context);
     this.player.update();
+    this.projectilesPool.forEach((projectile) => {
+      projectile.update();
+      projectile.draw(context);
+    });
+  }
+
+  // create projectiles object pool
+  createProjectiles() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+
+  // get free projectile from the pool
+  getProjectile() {
+    for (let i = 0; i < this.projectilesPool.length; i++) {
+      if (this.projectilesPool[i].free) return this.projectilesPool[i];
+    }
   }
 }
 
